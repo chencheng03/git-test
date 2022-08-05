e45be098222279f85026a66b7ce0f2e53a6451f3
chen <2871416765@qq.com>
2022-08-05 23:14:52 +0800

    test commit

--- src/main/java/com/cc/git/GitTest.java
+++ src/main/java/com/cc/git/GitTest.java
@@ -9,7 +9,10 @@ public class GitTest {
         System.out.println("my hot-fix222");
         System.out.println("push test");   
         System.out.println("pull test");
-                System.out.println("gitee test");
+        System.out.println("gitee test");
+
+
+        System.out.println("测试是不是同一个git本地库");
 
 
     }
